c547a23ff664cc565039f9839e3f4f310de28cb4
Abdul Basit <abdulbasit5361234@gmail.com>
2025-03-19 10:12:48 +0500

    add colors and the linear gradient brush

--- shared/src/commonMain/kotlin/musicapp/theme/Color.kt
+++ shared/src/commonMain/kotlin/musicapp/theme/Color.kt
@@ -1,2 +1,14 @@
 package musicapp.theme
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val lightPrimaryLighter = Color(0xFF8F89F8)
+val lightPrimary = Color(0xFF605ca8)
+
+val gradientBrush = Brush.linearGradient(
+    colors = listOf(lightPrimaryLighter, lightPrimary),
+    start = Offset(0f, 0f),
+    end = Offset(0f, Float.POSITIVE_INFINITY)
+)
