fd98aeec1863ad9c92336fa4ca128b03bd8a4bbb
Abdul Basit <abdulbasit5361234@gmail.com>
2025-03-21 13:46:31 +0500

    remove unnecessary koin stop

--- shared/src/iosMain/kotlin/musicapp/di/KoinHelper.kt
+++ shared/src/iosMain/kotlin/musicapp/di/KoinHelper.kt
@@ -4,13 +4,8 @@ import org.koin.core.context.startKoin
 
 object KoinHelper {
     fun initKoin() {
-        stopKoin()
         startKoin {
             modules(commonModule, iosModule)
         }
-    }
-
-    fun stopKoin() {
-        stopKoin()
     }
 }
