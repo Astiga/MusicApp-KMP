6b57a00e68bd223ef7c7eed737c01ef1c37b0cf3
Abdul Basit <abdulbasit5361234@gmail.com>
2025-03-21 09:26:47 +0500

    Refactor MainiOS function to initialize Koin and set status bar style
    The MainiOS function is refactored to:
    - Initialize Koin DI framework.
    - Set the status bar style to light content (white icons).
    - Update the column modifier to fill the max size.

--- shared/src/iosMain/kotlin/musicapp/main.ios.kt
+++ shared/src/iosMain/kotlin/musicapp/main.ios.kt
@@ -1,14 +1,9 @@
 package musicapp
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
@@ -22,41 +17,46 @@ import com.seiko.imageloader.component.setupDefaultComponents
 import com.seiko.imageloader.intercept.bitmapMemoryCacheConfig
 import com.seiko.imageloader.util.identityHashCode
 import musicapp.decompose.MusicRootImpl
-import musicapp.network.SpotifyApiImpl
-import musicapp.player.MediaPlayerController
-import musicapp.player.PlatformContext
+import musicapp.di.KoinHelper
+import platform.UIKit.UIApplication
+import platform.UIKit.UIStatusBarStyleLightContent
 import platform.UIKit.UIViewController
+import platform.UIKit.setStatusBarStyle
 
 fun MainiOS(
     lifecycle: LifecycleRegistry,
-): UIViewController = ComposeUIViewController(configure = { enforceStrictPlistSanityCheck = false }) {
-    val rootComponent = MusicRootImpl(
-        componentContext = DefaultComponentContext(lifecycle = lifecycle),
-        api = SpotifyApiImpl(),
-        mediaPlayerController = MediaPlayerController(PlatformContext())
-    )
+): UIViewController {
+    KoinHelper.initKoin()
 
-    Column(Modifier.background(color = Color(0xFF1A1E1F))) {
-        Box(
-            modifier = Modifier.fillMaxWidth().height(40.dp).background(color = Color(0xFF1A1E1F))
+    return ComposeUIViewController(configure = { 
+        enforceStrictPlistSanityCheck = false 
+    }) {
+        // Set status bar style for light content (white icons)
+        UIApplication.sharedApplication.setStatusBarStyle(UIStatusBarStyleLightContent)
+
+        val rootComponent = MusicRootImpl(
+            componentContext = DefaultComponentContext(lifecycle = lifecycle)
         )
-        CompositionLocalProvider(
-            LocalImageLoader provides ImageLoader {
-                components {
-                    setupDefaultComponents()
+
+        Column(Modifier.fillMaxSize()) {
+            CompositionLocalProvider(
+                LocalImageLoader provides ImageLoader {
+                    components {
+                        setupDefaultComponents()
+                    }
+                    interceptor {
+                        bitmapMemoryCacheConfig(
+                            valueHashProvider = { identityHashCode(it) },
+                            valueSizeProvider = { 500 },
+                            block = fun MemoryCacheBuilder<MemoryKey, Bitmap>.() {
+                                maxSizePercent(0.25)
+                            }
+                        )
+                    }
                 }
-                interceptor {
-                    bitmapMemoryCacheConfig(
-                        valueHashProvider = { identityHashCode(it) },
-                        valueSizeProvider = { 500 },
-                        block = fun MemoryCacheBuilder<MemoryKey, Bitmap>.() {
-                            maxSizePercent(0.25)
-                        }
-                    )
-                }
-            },
-        ) {
-            MainCommon(rootComponent, false)
+            ) {
+                MainCommon(rootComponent, false)
+            }
         }
     }
 }
