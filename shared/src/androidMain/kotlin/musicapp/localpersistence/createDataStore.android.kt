4398f06ecaa57fa397854b1297db0161b82361c8
Abdul Basit <abdulbasit5361234@gmail.com>
2025-03-24 15:20:55 +0500

    Android Context issue resolved
    using Koin to inject the `context`. Also, the code is updated to use `Any()` instead of `SynchronizedObject()`.

--- shared/src/androidMain/kotlin/musicapp/localpersistence/createDataStore.android.kt
+++ shared/src/androidMain/kotlin/musicapp/localpersistence/createDataStore.android.kt
@@ -1,18 +1,31 @@
 package musicapp.localpersistence
 
+import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.runBlocking
+import okio.Path.Companion.toPath
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+
+class DataStoreHelper : KoinComponent {
+    val context: Context by inject()
+}
 
 actual fun astigaDataStore(): DataStore<Preferences> {
+
     return runBlocking {
-        getDataStore(producePath = { ContextUtils.context.filesDir.resolve(dataStoreFileName).absolutePath })
+        val dataStoreHelper = DataStoreHelper()
+        getDataStore(producePath = { dataStoreHelper.context.filesDir.resolve(dataStoreFileName).absolutePath })
     }
 }
 
+
 private lateinit var dataStore: DataStore<Preferences>
 
-@OptIn(InternalCoroutinesApi::class)
-private val lock = SynchronizedObject()
+private val lock = Any()
 
 fun getDataStore(producePath: () -> String): DataStore<Preferences> =
     synchronized(lock) {
