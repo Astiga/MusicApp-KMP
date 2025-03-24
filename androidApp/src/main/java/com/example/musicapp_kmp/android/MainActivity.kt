05d1ec7cfcd057921ef2c1f228d248a775c7e45b
Abdul Basit <abdulbasit5361234@gmail.com>
2025-03-24 20:00:32 +0500

    enable edge-to-edge display in the android app

--- androidApp/src/main/java/com/example/musicapp_kmp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/musicapp_kmp/android/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.musicapp_kmp.android
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.view.WindowCompat
 import com.arkivanov.decompose.defaultComponentContext
 import musicapp.MainAndroid
@@ -16,7 +19,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         val api = SpotifyApiImpl()
         val root = MusicRootImpl(
             componentContext = defaultComponentContext(),
