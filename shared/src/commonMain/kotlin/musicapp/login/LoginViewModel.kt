4caa93c472ea125695a2a7ded047ebe34f877f1f
Abdul Basit <abdulbasit5361234@gmail.com>
2025-03-25 16:15:03 +0500

    Added localPersistenceComponents to LoginViewModel
    -add username and password to Datastore on successful login response

--- shared/src/commonMain/kotlin/musicapp/login/LoginViewModel.kt
+++ shared/src/commonMain/kotlin/musicapp/login/LoginViewModel.kt
@@ -1,27 +1,36 @@
 package musicapp.login
 
 import com.arkivanov.essenty.instancekeeper.InstanceKeeper
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import musicapp.localpersistence.LocalPersistenceComponents
 import musicapp.network.AstigaApi
 
 /**
  * ViewModel for handling login functionality.
  */
 class LoginViewModel(
-    private val astigaApi: AstigaApi
+    private val astigaApi: AstigaApi,
+    private val localPersistenceComponents: LocalPersistenceComponents
 ) : InstanceKeeper.Instance {
 
     private val coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
         exception.printStackTrace()
     }
 
+
     private val job = SupervisorJob()
     private val viewModelScope = CoroutineScope(Dispatchers.Main + coroutineExceptionHandler + job)
 
     private val _loginState = MutableStateFlow<LoginState>(LoginState.Initial)
     val loginState: StateFlow<LoginState> = _loginState
+
 
     /**
      * Attempts to login with the provided credentials.
@@ -43,15 +52,21 @@ class LoginViewModel(
                     if (pingResponse.subsonicResponse.status == "ok") {
                         astigaApi.getLicense().onSuccess { licenseResponse ->
                             _loginState.value = LoginState.Success
+                            localPersistenceComponents.writeUserNameAndPassword(username, password)
                         }.onFailure { error ->
-                            _loginState.value = LoginState.Error("License validation failed: ${error.message}")
+                            _loginState.value =
+                                LoginState.Error("License validation failed: ${error.message}")
                         }
+                    } else if (pingResponse.subsonicResponse.status == "40") {
+                        _loginState.value = LoginState.Error("This password or email is incorrect")
                     } else {
                         _loginState.value = LoginState.Error("Invalid username or password")
                     }
                 }.onFailure { error ->
-                    _loginState.value = LoginState.Error("Invalid username or password: ${error.message}")
+                    _loginState.value =
+                        LoginState.Error("Invalid username or password: ${error.message}")
                 }
+
             } catch (e: Exception) {
                 _loginState.value = LoginState.Error("An error occurred: ${e.message}")
             }
