4e8b11b65b19102955807df61f706ca1f22963b2
Abdul Basit <abdulbasit5361234@gmail.com>
2025-03-25 15:27:16 +0500

    update the common module to include datastore and local persistence components

--- shared/src/commonMain/kotlin/musicapp/di/CommonModule.kt
+++ shared/src/commonMain/kotlin/musicapp/di/CommonModule.kt
@@ -1,10 +1,15 @@
 package musicapp.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import musicapp.decompose.MusicRootImpl
 import musicapp.decompose.chartdetails.ChartDetailsComponent
 import musicapp.decompose.chartdetails.ChartDetailsComponentImpl
 import musicapp.decompose.dashboard.DashboardMainComponent
 import musicapp.decompose.dashboard.DashboardMainComponentImpl
-import musicapp.decompose.MusicRootImpl
+import musicapp.localpersistence.LocalPersistenceComponents
+import musicapp.localpersistence.LocalPersistenceImp
+import musicapp.localpersistence.astigaDataStore
 import musicapp.network.AstigaApi
 import musicapp.network.AstigaApiImpl
 import musicapp.network.SpotifyApi
@@ -12,9 +17,11 @@ import musicapp.network.SpotifyApiImpl
 import org.koin.dsl.module
 
 val commonModule = module {
-    single<SpotifyApi> { 
+    single<SpotifyApi> {
         SpotifyApiImpl()
     }
+    single<DataStore<Preferences>> { astigaDataStore() }
+    single<LocalPersistenceComponents> { LocalPersistenceImp(get()) }
 
     single<AstigaApi> {
         AstigaApiImpl()
